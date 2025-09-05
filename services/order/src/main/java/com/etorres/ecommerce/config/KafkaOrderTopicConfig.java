4349ec32e197d5e09e4b56a51b13bb3b0cff2cfe
Snowsita <Snowsita@users.noreply.github.com>
2025-09-05 13:22:08 -0600

    chore: Dependencies added

---
+++ services/order/src/main/java/com/etorres/ecommerce/config/KafkaOrderTopicConfig.java
@@ -0,0 +1,11 @@
+package com.etorres.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaOrderTopicConfig {
+
+    // @Bean
+    // public NewTopic
+}
